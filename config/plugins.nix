9b80f41d6ed71d53e5bcacd9d7bbcdc1d5940f54
vmorganp <31448722+vmorganp@users.noreply.github.com>
2024-04-05 13:55:49 -0700

    add harpoon keys

--- config/plugins.nix
+++ config/plugins.nix
@@ -28,7 +28,22 @@
       };
     };
     # TODO add keybinds
-    harpoon.enable = true;
+    harpoon = {
+      enable = true;
+      keymaps = {
+        addFile = "<leader>ha";
+        toggleQuickMenu = "<leader>hs";
+        navFile = {
+          "1" = "<leader>h1";
+          "2" = "<leader>h2";
+          "3" = "<leader>h3";
+          "4" = "<leader>h4";
+          "5" = "<leader>h5";
+        };
+
+      };
+    };
+
     chadtree.enable = true;
 
     markdown-preview.enable = true;
