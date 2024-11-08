17a1d22c2810399bacc54ac147973d4ec0696e99
vmorganp <31448722+vmorganp@users.noreply.github.com>
2024-11-08 11:01:43 -0700

    add web tech lsps

--- config/lsp.nix
+++ config/lsp.nix
@@ -5,11 +5,18 @@
       servers = {
         gopls.enable = true;
         golangci_lint_ls.enable = true;
+        templ.enable = true;
         lua_ls.enable = true;
         pyright.enable = true;
         terraformls.enable = true;
         ts_ls.enable = true;
+
+        # Web stuff
         svelte.enable = true;
+        html.enable = true;
+        htmx.enable = true;
+        cssls.enable = true;
+        eslint.enable = true;
 
         rust_analyzer = {
           enable = true;
